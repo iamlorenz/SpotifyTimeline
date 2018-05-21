60031d1e4fff5c240529dee14d5f677022c2f912
Lorenz Schimik <schimik@outlook.com>
2018-05-21 15:19:42 +0200

    load album info on click

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import SpotifyWebApi from 'spotify-web-api-js';
 import Timeline from 'react-visjs-timeline';
 import './App.css';
 import Options from './options';
+// import { Sidebar } from './sidebar';
 const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     const params = this.getHashParams();
     const token = params.access_token;
     if (token) {
@@ -17,7 +18,9 @@ class App extends Component {
     this.state = {
       loggedIn: token ? true : false,
       profile: { name: '' },
-      albums: [{start: "2017-12-18T20:44:02Z", content: "" }]
+      albums: [{start: "2017-12-18T20:44:02Z", content: "" }],
+      selectedAlbum: {},
+      showSidebar: false
     }
   }
 
@@ -57,13 +60,13 @@ class App extends Component {
         for (let i = 0; i < response.items.length; i++) {
           const albumArt = response.items[i].album.images[0].url;
           const album = {
+            id: response.items[i].album.id,
             start: response.items[i].added_at,
             content: "<img style='width: 100px; height: 100px;' src='"+albumArt+"'/>",
             title: "<b>"+response.items[i].album.artists[0].name + "</b><br>" + response.items[i].album.name
           }
           userAlbums.push(album);
         }
-
         this.setState({
           albums: userAlbums
         });
@@ -82,9 +85,11 @@ class App extends Component {
         <Timeline
           options={Options}
           items={this.state.albums}
+          clickHandler={loadTracksforAlbum}
         />
 
         <div className="sidebar">
+
         </div>
 
       </div>
@@ -93,3 +98,24 @@ class App extends Component {
 }
 
 export default App;
+
+// LOAD TRACK LIST
+function loadTracksforAlbum(props) {
+  if (props.item !== null){
+    spotifyApi.getAlbum(props.item)
+      .then((response) => {
+
+        console.log(response);
+
+        // this.setState({
+        //   album: {
+        //     name: response.name,
+        //     artist: response.artists[0],
+        //     tracks: response.tracks.items
+        //   }
+        // });
+
+
+      })
+  }
+}
